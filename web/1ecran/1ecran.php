a55c9fa3e8feee0003dcc5ca755626296aef5e58
Lea <marielea06370@gmail.com>
2023-03-20 12:31:52 +0100

    liaison bd et choix avec 1 ecran pour une personne

---
+++ web/1ecran/1ecran.php
@@ -0,0 +1,109 @@
+﻿<?php
+session_start();
+// Connexion à la base de données MySQL
+require('../bd.php');
+$bdd = getBD();
+
+// Récupération des données pour le menu "Genre 1"
+$sql = "SELECT genre FROM genre_new";
+$stmt = $bdd->query($sql);
+$options_genre_1 = '<option>Pas de préférence</option>';
+while ($row = $stmt->fetch()) {
+    $options_genre_1 .= '<option>' . htmlspecialchars($row['genre']) . '</option>';
+}
+
+// Récupération des données pour le menu "Genre 2"
+$sql = "SELECT genre FROM genre_new";
+$stmt = $bdd->query($sql);
+$options_genre_2 = '<option>Pas de préférence</option>';
+while ($row = $stmt->fetch()) {
+    $options_genre_2 .= '<option>' . htmlspecialchars($row['genre']) . '</option>';
+}
+
+// Récupération des données pour le menu "Director"
+$sql = "SELECT director FROM director_new";
+$stmt = $bdd->query($sql);
+$options_director = '';
+while ($row = $stmt->fetch()) {
+    $options_director .= '<option>' . htmlspecialchars($row['director']) . '</option>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Un seul écran</title>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+    <div class="logo">
+        <img src="../images/logo.png" alt="Logo">
+    </div>
+</header>
+
+<p class="prem">
+    Ajouter le nombre d'utilisateurs souhaité puis remplisser vos critères :
+</p>
+<div class="container">
+<form action="enregistrementchoix_1ecran.php" method="post">
+    <div class="row">
+      <div class="col-md-4 text-start">
+        <img src="../images/images.png" alt="IMG" title="User1" width="25%">
+      </div>
+    <div class="col-md-8">
+        <div class="row">
+          <div class="col-md-2">
+            <select class="form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="type">
+              <option>Type</option>
+              <option>Movie</option>
+              <option>TV Show</option>
+            </select>
+          </div>
+          <div class="col-md-2">
+            <select class="form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="genre1">
+              <option>Genre 1</option>
+              <?php echo $options_genre_1; ?>
+            </select>
+          </div>
+          <div class="col-md-2">
+            <select class="form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="genre2">
+              <option>Genre 2</option>
+              <?php echo $options_genre_2; ?>
+            </select>
+          </div>
+          <div class="col-md-2">
+            <select class="form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="director">
+                <option>Réalisateur</option>
+                <?php echo $options_director; ?>
+            </select>
+          </div>
+          <div class="col-md-2">
+                <button class="btn btn-primary rounded-0 w-100 font-weight-bold" type="submit" name="submit">Recherche</button>
+          </div>
+        </div>
+        </form>
+    </div>
+</div>
+</div>
+
+<div class='ml-auto'>
+    <a href="1ecran-user2.php">
+        <button class="btn btn-light rounded-circle">
+            <span class="font-weight-bold">+</span>
+        </button>
+        <span class="ml-2 text-white font-weight-bold">Ajouter un utilisateur</span>
+    </a>
+</div>
+</div>
+
+<div class="fixed-bottom text-right mr-3 mb-3">
+  <button type="button" class="btn btn-light rounded-pill text-dark font-weight-bold btn-lg" onclick="window.location.href='../index.html'">
+    >>> 
+  </button>
+</div>
+</body>
+</html>
---
+++ web/1ecran/enregistrementChoix1ecran.php
@@ -0,0 +1,91 @@
+﻿<?php
+session_start();
+// Connexion à la base de données MySQL
+require('../bd.php');
+$bdd = getBD();
+
+if (isset($_POST['submit'])) {
+    // Récupération des données du formulaire
+    $type = $_POST['type'];
+    $genre1 = $_POST['genre1'];
+    $genre2 = $_POST['genre2'];
+    $director = $_POST['director'];
+    $date = date('Y-m-d H:i:s');
+    if(isset($_SESSION['utilisateur'])) {
+        $id_us = $_SESSION['utilisateur']['id'];
+    } else {
+        // rediriger l'utilisateur vers une page de connexion
+        header('Location: connexion.php');
+        exit();
+    }
+
+    //Récupération de id_direc
+    $stmt = $bdd->prepare('SELECT id_direc FROM director_new WHERE director = ?');
+    $stmt->execute([$director]);
+    $id_direc = $stmt->fetchColumn();
+
+    //Récupération de id_genre1
+    $stmt = $bdd->prepare('SELECT id_genre FROM genre_new WHERE genre = ?');
+    $stmt->execute([$genre1]);
+    $id_genre1 = $stmt->fetchColumn();
+
+    //Récupération de id_genre2
+    $stmt = $bdd->prepare('SELECT id_genre FROM genre_new WHERE genre = ?');
+    $stmt->execute([$genre2]);
+    $id_genre2 = $stmt->fetchColumn();
+
+    // Insertion des données dans les tables CHOIX
+    $sql = "INSERT INTO choix (ordre, date, type) VALUES (?,?,?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([1, $date, $type]);
+
+    //Recherche de l'identifiant du choix
+    $stmt = $bdd->prepare('SELECT id_choix FROM choix WHERE date = ?');
+    $stmt->execute([$date]);
+    $id_choix = $stmt->fetchColumn();
+
+
+    // Insertion des données dans les tables de liaison faire et avoird et avoirg
+    $sql = "INSERT INTO faire (id_us, id_choix) VALUES ($id_us, $id_choix)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_us, $id_choix]);
+
+    $sql = "INSERT INTO avoird (id_direc, id_choix) VALUES (?, ?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_direc, $id_choix]);
+
+    $sql = "INSERT INTO avoirg (id_genre, id_choix) VALUES (?, ?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_genre1, $id_choix]);
+
+    // Deuxième fois pour genre2
+    // Insertion des données dans la table CHOIX
+    $sql = "INSERT INTO choix (ordre, date, type) VALUES (?,?,?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([1, $date, $type]);
+
+    //Recherche de l'identifiant du choix
+    $stmt = $bdd->prepare('SELECT id_choix FROM choix WHERE date = ? AND id_choix = ?');
+    $stmt->execute([$date, $id_choix +1]);
+    $id_choix2 = $stmt->fetchColumn();
+
+
+    // Insertion des données dans les tables de liaison faire et avoird et avoirg
+    $sql = "INSERT INTO faire (id_us, id_choix) VALUES ($id_us, $id_choix2)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_us, $id_choix2]);
+
+    $sql = "INSERT INTO avoird (id_direc, id_choix) VALUES (?, ?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_direc, $id_choix2]);
+
+    $sql = "INSERT INTO avoirg (id_genre, id_choix) VALUES (?, ?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_genre2, $id_choix2]);
+
+
+    // Redirection vers la page de confirmation
+    header("Location: ../contenus1.html");
+    exit();
+}
+?>
