4ff9d1f1970b5a857cba1c82677e70cb3c2317bb
Lea <marielea06370@gmail.com>
2023-03-21 11:48:21 +0100

    ajouter dans la table acceder a une salle

--- web/rejoindreSalle/codeForm.php
+++ web/rejoindreSalle/codeForm.php
@@ -5,6 +5,14 @@ if (isset($_GET['error']) && $_GET['error'] == 1) {
     echo '<script>alert("Le code entré n\'est pas valide. Veuillez réessayer.")</script>';
 }
 ?>
+<?php
+session_start();
+// Vérifier si une erreur a été signalée dans l'URL
+if (isset($_GET['error']) && $_GET['error'] == 1) {
+    // Afficher une alerte
+    echo '<script>alert("Le code entré n\'est pas valide. Veuillez réessayer.")</script>';
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -12,22 +20,22 @@ if (isset($_GET['error']) && $_GET['error'] == 1) {
 		<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" media="screen"/>
 		<meta charset="UTF-8">>
 	</head>
-		<body>
-			<header>
-				<div class="logo">
-					<img src="../images/logo.png" alt="Logo">
-				</div>
-			</header>
-				<form action="./verifCode.php" method="post" autocomplete="off">
-					<div class="codeForm">
-						<label for="Code">Rentrez votre code de salle :</label>
-						<input type="text" class="form-control" id="Code" name="Code">
-					</div>
-						<div class="fixed-bottom text-right mr-3 mb-3">
-							<button type="submit" class="btn btn-light rounded-pill text-dark font-weight-bold btn-lg">
-								>>>
-							</button>
-						</div>
-					</form>
-				</body>
-			</html>
+	<body>
+	<header>
+	<div class="logo">
+		<img src="../images/logo.png" alt="Logo">
+	</div>
+	</header>
+	<form action="./verifCode.php" method="post" autocomplete="off">
+		<div class="codeForm">
+			<label for="Code">Rentrez votre code de salle :</label>
+			<input type="text" class="form-control" id="Code" name="Code">
+		</div>
+		<div class="fixed-bottom text-right mr-3 mb-3">
+			<button type="submit" class="btn btn-light rounded-pill text-dark font-weight-bold btn-lg">
+			>>>
+			</button>
+		</div>
+	</form>
+	</body>
+</html>
--- web/rejoindreSalle/verifCode.php
+++ web/rejoindreSalle/verifCode.php
@@ -1,4 +1,5 @@
 ﻿<?php
+session_start();
 // Récupérer le code entré par l'utilisateur
 $code = $_POST['Code'];
 
@@ -16,7 +17,20 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Vérifier si le code existe dans la table
 if (count($rows) > 0) {
-    // Le code existe dans la table, rediriger vers la page user.html
+    // Le code existe dans la table, enregistrer l'accès dans la table "acceder"
+    if(isset($_SESSION['utilisateur'])) {
+        $id_us = $_SESSION['utilisateur']['id'];
+    } else {
+        // rediriger l'utilisateur vers une page de connexion
+        header('Location: ../connexion.php');
+        exit();
+    }
+    $date = date("Y-m-d H:i:s");
+    $sql = "INSERT INTO acceder(id_us, code, date) VALUES (?, ?, ?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$id_us, $code, $date]);
+
+    // Rediriger vers la page user.php
     header('Location: ./user.php');
     exit;
 } else {
@@ -29,3 +43,4 @@ if (count($rows) > 0) {
 $stmt->closeCursor();
 $conn = null;
 ?>
+
