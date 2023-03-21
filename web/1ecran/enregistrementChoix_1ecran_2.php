17d815fd040f1e807c31d6dec1052fbb73c3c601
Lea <marielea06370@gmail.com>
2023-03-21 09:36:29 +0100

    connexion bd et 1ecran

--- web/1ecran/enregistrementChoix_1ecran_2.php
+++ web/1ecran/enregistrementChoix_1ecran_2.php
@@ -19,11 +19,6 @@ if (isset($_POST['submit'])) {
         exit();
     }
 
-    //Récupération de id_direc
-    $stmt = $bdd->prepare('SELECT id_direc FROM director_new WHERE director = ?');
-    $stmt->execute([$director]);
-    $id_direc = $stmt->fetchColumn();
-
     //Récupération de id_genre1
     $stmt = $bdd->prepare('SELECT id_genre FROM genre_new WHERE genre = ?');
     $stmt->execute([$genre1]);
@@ -50,10 +45,6 @@ if (isset($_POST['submit'])) {
     $stmt = $bdd->prepare($sql);
     $stmt->execute([$id_us, $id_choix]);
 
-    $sql = "INSERT INTO avoird (id_direc, id_choix) VALUES (?, ?)";
-    $stmt = $bdd->prepare($sql);
-    $stmt->execute([$id_direc, $id_choix]);
-
     $sql = "INSERT INTO avoirg (id_genre, id_choix) VALUES (?, ?)";
     $stmt = $bdd->prepare($sql);
     $stmt->execute([$id_genre1, $id_choix]);
@@ -66,7 +57,7 @@ if (isset($_POST['submit'])) {
 
     //Recherche de l'identifiant du choix
     $stmt = $bdd->prepare('SELECT id_choix FROM choix WHERE date = ? AND id_choix = ?');
-    $stmt->execute([$date, $id_choix +1]);
+    $stmt->execute([$date, $id_choix + 1]);
     $id_choix2 = $stmt->fetchColumn();
 
 
@@ -74,10 +65,6 @@ if (isset($_POST['submit'])) {
     $sql = "INSERT INTO faire (id_us, id_choix) VALUES ($id_us, $id_choix2)";
     $stmt = $bdd->prepare($sql);
     $stmt->execute([$id_us, $id_choix2]);
-
-    $sql = "INSERT INTO avoird (id_direc, id_choix) VALUES (?, ?)";
-    $stmt = $bdd->prepare($sql);
-    $stmt->execute([$id_direc, $id_choix2]);
 
     $sql = "INSERT INTO avoirg (id_genre, id_choix) VALUES (?, ?)";
     $stmt = $bdd->prepare($sql);
@@ -87,6 +74,6 @@ if (isset($_POST['submit'])) {
     // Redirection vers la page de confirmation
     header('Location: ' . $_SERVER['HTTP_REFERER']);
     exit();
+
 }
 ?>
-
