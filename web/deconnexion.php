a7085304f45e321363d52e7e7136d1a07f0a5e99
leaam06 <marielea06370@gmail.com>
2023-03-28 12:15:20 +0200

    page deconnexion.php

--- web/deconnexion.php
+++ web/deconnexion.php
@@ -1,6 +1,6 @@
 ﻿<?php
 session_start(); // Démarre la session en cours
 session_destroy(); // Détruit toutes les données de la session
-header("Location: index.php"); // Redirige l'utilisateur vers la page d'accueil
+header("Location: 1.html"); // Redirige l'utilisateur vers la page d'accueil
 exit; // Met fin au script
 ?>
