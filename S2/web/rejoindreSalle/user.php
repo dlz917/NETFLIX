345315054790e78abfab9a3fb8c0329196803d98
kass <martkassandra@gmail.com>
2023-04-11 17:41:13 +0200

    changement avec ajout du formulaire pour le chat

--- S2/web/rejoindreSalle/user.php
+++ S2/web/rejoindreSalle/user.php
@@ -1,10 +1,11 @@
-﻿<?php
+<?php
 session_start();
 // Connexion à la base de données MySQL
 require('../bd.php');
 $bdd = getBD();
+$code = $_SESSION['code'];
 
-// Récupération des données pour le menu "Genre"
+// Récupération des données pour le menu "Genre 1"
 $sql = "SELECT genre FROM genre_new";
 $stmt = $bdd->query($sql);
 $options_genre = '';
@@ -27,83 +28,34 @@ $options_director = '';
 while ($row = $stmt->fetch()) {
     $options_director .= '<option>' . htmlspecialchars($row['director']) . '</option>';
 }
-
-// Vérifie si l'utilisateur est connecté
-if(isset($_SESSION['utilisateur'])) {
-    $id_us = $_SESSION['utilisateur']['id'];
-    
-    // Vérifie si l'utilisateur a une salle en cours
-    $stmt = $bdd->prepare('SELECT code FROM salle WHERE en_cours = true');
-    $stmt->execute();
-    $code_exist = $stmt->fetchColumn();
-    
-    if (!$code_exist) {
-        // Génère un code aléatoire unique de 4 chiffres
-        $code = strval(mt_rand(1000, 9999));
-
-        // Vérifie si le code existe déjà dans la table salle
-        $stmt = $bdd->prepare('SELECT code FROM salle WHERE code = :code');
-        $stmt->execute(array('code' => $code));
-
-        // Si le code existe déjà, en génère un nouveau jusqu'à ce qu'il soit unique
-        while ($stmt->fetch()) {
-            $code = strval(mt_rand(1000, 9999));
-            $stmt->execute(array('code' => $code));
-        }
-
-        // Insère le code, le nom d'utilisateur et la date actuelle dans la table salle
-        $date = date('Y-m-d H:i:s');
-        $stmt = $bdd->prepare('INSERT INTO salle (code, date, en_cours) VALUES (:code, :date, true)');
-        $stmt->execute(array('code' => $code, 'date' => $date));
-
-        $stmt = $bdd->prepare('INSERT INTO creer (id_us, code, date) VALUES (:id_us, :code, :date)');
-        $stmt->execute(array('id_us' => $id_us, 'code' => $code, 'date' => $date));
-    } else {
-        // Utilise le code existant
-        $code = $code_exist;
-    }
-} else {
-    // Rediriger l'utilisateur vers une page de connexion
-    header('Location: ../connexion.php');
-    exit();
-}
-
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
     <title>Un seul écran</title>
     <link href="../css/bootstrap.css" rel="stylesheet">
-
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
+
 <header>
 	<a href="../index.php"><div class="logo">
 		<img src="../images/logo.png" alt="Logo">
 		</div></a>
-    <a href="../deconnexion.php" style="color:white; position:absolute; top:80px; right:0; padding:20px;">Déconnexion</a>
+	<a href="../deconnexion.php" style="color:white; position:absolute; top:0; right:0; padding:20px;">Déconnexion</a>
 </header>
 
 <p class="prem">
-    Envoyer votre code salle aux utilisateurs souhaités puis remplisser vos critères :
+    Remplissez vos critères :
 </p>
-<div class="container">
-<form action="../1ecran/enregistrementChoix1ecran.php?ordre=2" method="post">
+
+<div class="container adduser">
+<form action="../1ecran/enregistrementChoix1ecran.php" method="post">
     <div class="row">
       <div class="col-md-4 text-start">
-        <img src="../images/images.png" alt="IMG" title="User1" width="25%">
+        <img src="../images/user2.png" alt="IMG" title="User2" width="25%">
       </div>
-    <div class="col-md-8">
-        <div class="row">
-          <div class="col-md-2">
-            <select class="form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="type">
-              <option>Type</option>
-              <option>Movie</option>
-              <option>TV Show</option>
-            </select>
-          </div>
           <div class="col-md-2">
             <select id="menu-1" class="mb-4 form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="genre[]">
               <option>Genre</option>
@@ -124,8 +76,8 @@ if(isset($_SESSION['utilisateur'])) {
           </div>
           <div class="col-md-2">
             <select id="menu-3" class="mb-4 form-control w-100 me-2 rounded-0 custom-select text-dark font-weight-bold" name="director[]">
-                <option>Réalisateur</option>
-                <?php echo $options_director; ?>
+              <option>Réalisateur</option>
+              <?php echo $options_director; ?>
             </select>
             <button id="btn-3" type="button" class="add-button btn btn-light rounded-circle">
                 <span class="font-weight-bold">+</span>
@@ -134,18 +86,7 @@ if(isset($_SESSION['utilisateur'])) {
           <div class="fixed-bottom text-right mr-3 mb-3">
               <button type="submit" class="btn btn-light rounded-pill text-dark font-weight-bold btn-lg">>>></button>
           </div>
-        </div>
-        </form>
-    </div>
-</div>
-</div>
-<div id="user-choice">
-</div>
-
-<div class="fixed-top text-right mr-3 mb-3" style="margin-top: 30px; margin-right:50px;">
-    <span style="color: white; font-weight: bold;">Votre code :</span>
-    <div class="code-box bg-light d-inline-flex align-items-center justify-content-center">
-        <span class="code-text font-weight-bold"><?php echo $code; ?></span>
+          </form>
     </div>
 </div>
 
@@ -224,10 +165,106 @@ if(isset($_SESSION['utilisateur'])) {
         counter++;
         btn3.querySelector('span').textContent = '+';
         });
-
+ $(function() {
+            setInterval(function() {
+                $.ajax({
+                    url: '../creerSalle/affiche_message.php',
+                    type: 'GET',
+                    success: function(data) {
+                        $('#messagesInviter').html(data);
+                    }
+                });
+            }, 5000);
+        });
     </script>
 
 <?php
+// Récupération des choix de genre de l'admin de la salle
+$sql = "SELECT utilisateur.id_us, genre_new.genre
+        FROM creer, utilisateur, faire, choix, avoirg, genre_new
+        WHERE creer.code = :code
+        AND creer.id_us = utilisateur.id_us
+        AND utilisateur.id_us = faire.id_us
+        AND faire.id_choix = choix.id_choix
+        AND choix.id_choix = avoirg.id_choix
+        AND avoirg.id_genre = genre_new.id_genre";
+$stmt = $bdd->prepare($sql);
+$stmt->execute(array('code' => $code));
+$admin_choices_genre = $stmt->fetchAll();
+
+// Récupération des choix de cast de l'admin de la salle
+$sql = "SELECT utilisateur.id_us, cast_new.cast
+        FROM creer, utilisateur, faire, choix, avoirc, cast_new
+        WHERE creer.code = :code
+        AND creer.id_us = utilisateur.id_us
+        AND utilisateur.id_us = faire.id_us
+        AND faire.id_choix = choix.id_choix
+        AND choix.id_choix = avoirc.id_choix
+        AND avoirc.id_cast = cast_new.id_cast";
+$stmt = $bdd->prepare($sql);
+$stmt->execute(array('code' => $code));
+$admin_choices_cast = $stmt->fetchAll();
+
+// Récupération des choix de director de l'admin de la salle
+$sql = "SELECT utilisateur.id_us, director_new.director
+        FROM creer, utilisateur, faire, choix, avoird, director_new
+        WHERE creer.code = :code
+        AND creer.id_us = utilisateur.id_us
+        AND utilisateur.id_us = faire.id_us
+        AND faire.id_choix = choix.id_choix
+        AND choix.id_choix = avoird.id_choix
+        AND avoird.id_direc = director_new.id_direc";
+$stmt = $bdd->prepare($sql);
+$stmt->execute(array('code' => $code));
+$admin_choices_director = $stmt->fetchAll();
+
+//Récupération de l'e-mail l'admin de la salle
+$sql = "SELECT utilisateur.id_us, utilisateur.mail
+        FROM creer, utilisateur
+        WHERE creer.code = :code
+        AND creer.id_us = utilisateur.id_us";
+$stmt = $bdd->prepare($sql);
+$stmt->execute(array('code' => $code));
+$admin_email = $stmt->fetchAll();
+
+if ($admin_choices_genre && $admin_choices_cast && $admin_choices_director) {
+    // Affichage des choix de l'utilisateur
+    echo '<div class="container">';
+    echo '<p class="prem">Choix de l\'administrateur de votre salle:</p>';
+
+    foreach ($admin_email as $admin) {
+        // Affichage de l'utilisateur et des choix
+        echo '<div class="row mb-3"><div class="col-md-2"><img src="../images/images.png" alt="IMG" title="adminIMG" width="50%"></div>';
+        echo '<div class="col-md-8"><div class="row"><div class="col-md-4"><p class="prem">Utilisateur: ' . $admin['id_us'] . ' - ' . $admin['mail'] . '</p></div>';
+        echo '<div class="col-md-8"><p class="prem">Genres: ';
+        $genres = array();
+        $actors = array();
+        $directors = array();
+        foreach ($admin_choices_genre as $choice) {
+            if ($choice['id_us'] == $admin['id_us']) {
+                $genres[] = $choice['genre'];
+            }
+        }
+        echo implode(', ', $genres) . '<br>';
+        echo 'Acteurs: ';
+        foreach ($admin_choices_cast as $choice_cast) {
+            if ($choice_cast['id_us'] == $admin['id_us']) {
+                $actors[] = $choice_cast['cast'];
+            }
+        }
+        echo implode(', ', $actors) . '<br>';
+        echo 'Réalisateurs: ';
+        foreach ($admin_choices_director as $choice_director) {
+            if ($choice_director['id_us'] == $admin['id_us']) {
+                $directors[] = $choice_director['director'];
+            }
+        }
+        echo implode(', ', $directors) . '</p></div></div></div></div>';
+    }
+    echo '</div>';
+} else {
+    echo 'Aucun choix d\'utilisateur n\'a été trouvé.';
+}
 
 // Récupération des choix de genre des utilisateurs connectés à la salle
 $sql = "SELECT utilisateur.id_us, genre_new.genre
@@ -236,7 +273,6 @@ $sql = "SELECT utilisateur.id_us, genre_new.genre
         AND acceder.id_us = utilisateur.id_us
         AND utilisateur.id_us = faire.id_us
         AND faire.id_choix = choix.id_choix
-        AND choix.ordre = 2
         AND choix.id_choix = avoirg.id_choix
         AND avoirg.id_genre = genre_new.id_genre";
 $stmt = $bdd->prepare($sql);
@@ -250,7 +286,6 @@ $sql = "SELECT utilisateur.id_us, cast_new.cast
         AND acceder.id_us = utilisateur.id_us
         AND utilisateur.id_us = faire.id_us
         AND faire.id_choix = choix.id_choix
-        AND choix.ordre = 2
         AND choix.id_choix = avoirc.id_choix
         AND avoirc.id_cast = cast_new.id_cast";
 $stmt = $bdd->prepare($sql);
@@ -264,7 +299,6 @@ $sql = "SELECT utilisateur.id_us, director_new.director
         AND acceder.id_us = utilisateur.id_us
         AND utilisateur.id_us = faire.id_us
         AND faire.id_choix = choix.id_choix
-        AND choix.ordre = 2
         AND choix.id_choix = avoird.id_choix
         AND avoird.id_direc = director_new.id_direc";
 $stmt = $bdd->prepare($sql);
@@ -321,6 +355,25 @@ if ($users_choices_genre && $users_choices_cast && $users_choices_director) {
 }
 
 ?>
+
+					<!-- Le forumulaire du tchat-->
+
+ 			<form action="../CreerSalle/envoyer_message.php" method="POST">
+ 			
+ 			<input type="text" name="pseudo" id="pseudo" placeholder="pseudo" value="<?php if(isset($_SESSION['pseudo'])) { echo $_SESSION['pseudo']; } ?>">
+ 			<br><br>
+ 			 <textarea name="texte" ></textarea>
+ 			 <br>
+ 			<!--<input type="text" id="texte" name ="texte" placeholder= " type in your message">-->
+ 			
+ 			<input type="submit" name="valider"> 
+
+ 			</form>
+ 			<div id="messagesInviter">
+        <!-- Le contenu de la page sera mis à jour ici -->
+    </div>
+ 			<!-- appel de la page affichage message pour afficher message -->
+ 			
 
 </body>
 </html>
