3256269ca1f85add4951620f09f3f2c00fb147fd
leaam06 <marielea06370@gmail.com>
2023-04-29 18:06:36 +0200

    Modification dans enregistrementNchoice qui permet de connecter l'utilisateur car avant même si l'utilisateur n'existait pas dans la bd il pouvait se connecter donc ajout d'une condition

--- S2/web/enregistrementNchoice.php
+++ S2/web/enregistrementNchoice.php
@@ -8,39 +8,46 @@
 		session_start();
 		
 		function connecter($email, $password) {
-		require('bd.php');
-		$bdd = getBD();
-		$req = $bdd->prepare('SELECT * FROM Utilisateur WHERE mail = :email ');
-		$req->bindParam(':email', $email);
-		$req->execute(array('email' => $email));	
-		$utilisateur = $req->fetch();
+			require('bd.php');
+			$bdd = getBD();
 	
-		if (isset($_POST['mail']) && !empty($_POST['mail']) && isset($_POST['mdp']) && !empty($_POST['mdp'])) {
-			$email = $_POST['mail'];
-			$mdp = md5($_POST['mdp']);
-			$_SESSION['utilisateur'] = array(
-			'id' => $utilisateur['id_us'],
-			'mail' => $email
-			);
-			header('Location: index.php');
-			exit();
-	} else {
-		header('Location: connexion.php?mail='.$_POST["mail"]);
-		exit();
-}}
+			if (isset($_POST['mail']) && !empty($_POST['mail']) && isset($_POST['mdp']) && !empty($_POST['mdp'])) {
+				$req = $bdd->prepare('SELECT * FROM Utilisateur WHERE mail = :email ');
+				$req->bindParam(':email', $email);
+				$req->execute(array('email' => $email));	
+				$utilisateur = $req->fetch();
+
+				$mdp = md5($_POST['mdp']);
+				if ($utilisateur && $mdp == $utilisateur['mdp']) {
+					$_SESSION['utilisateur'] = array(
+						'id' => $utilisateur['id_us'],
+						'mail' => $email
+					);
+					header('Location: index.php');
+					exit();
+				} else {
+					header('Location: connexion.php?mail='.$_POST["mail"]);
+					exit();
+				}
+			} else {
+				header('Location: connexion.php?mail='.$_POST["mail"]);
+				exit();
+			}
+		}
+
 		
-    if (isset($_POST['mail']) && empty($_POST['mail']) || isset($_POST['mdp']) && empty($_POST['mdp'])){
+		if (isset($_POST['mail']) && empty($_POST['mail']) || isset($_POST['mdp']) && empty($_POST['mdp'])){
 		 
-		// si il manque des données retourne sur page nouveau 
-		echo "<meta http-equiv=\"refresh\" content=\"0; url=connexion.php?mail=".$_POST['mail']."\">";
-		} 
-	else {
-	// tout complet appel fonction connecter
-		$email = $_POST['mail'];
-		$mdp=$_POST['mdp'];
-		connecter($email, $mdp);
+			// si il manque des données retourne sur page nouveau 
+			echo "<meta http-equiv=\"refresh\" content=\"0; url=connexion.php?mail=".$_POST['mail']."\">";
+			} 
+		else {
+		// tout complet appel fonction connecter
+			$email = $_POST['mail'];
+			$mdp=$_POST['mdp'];
+			connecter($email, $mdp);
 		
-	}
+		}
         ?>
         </head>
         <body>
